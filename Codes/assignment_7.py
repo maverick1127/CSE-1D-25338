3334d67fd2aa2132a25e56204133fee6142a2c69
maverick1127 <s.mithuntharan1127@gmail.com>
2025-09-23 08:55:14 +0530

    changed math  module usage

--- Codes/assignment_7.py
+++ Codes/assignment_7.py
@@ -1,9 +1,9 @@
-from math import sqrt
+
 
 a = int(input('Enter co-efficient of x^2: '))
 b = int(input('Enter co-efficient of x: '))
 c = int(input('Enter co-efficient of constant: '))
 
-val1, val2 = ((-b + sqrt(b**2 - 4*a*c))/2, (-b - sqrt(b**2 - 4*a*c))/2)
+val1, val2 = ((-b + (b**2 - 4*a*c) ** 1/2 )/2, (-b - (b**2 - 4*a*c)** 1/2)/2)
 
 print(f'The roots of the quadratic equations are {val1} and {val2}')
